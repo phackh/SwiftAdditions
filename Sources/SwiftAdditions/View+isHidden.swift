e4d3aa5d900beadfc78e94fd74e99663d827f03d
Patrick Hackh <mail@phackh.de>
2022-09-24 13:43:49 +0200

    View+isHidden

---
+++ Sources/SwiftAdditions/View+isHidden.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+@available(macOS 12, iOS 16, *)
+extension View {
+	@ViewBuilder func isHidden(_ hidden: Bool, remove: Bool = false) -> some View {
+		if hidden {
+			if !remove { self.hidden() }
+		}
+		else {
+			self
+		}
+	}
+}

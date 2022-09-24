96feb1dc88a1902991a684ec5fd55ac007f5eb6e
Patrick Hackh <mail@phackh.de>
2022-09-24 13:52:58 +0200

    BorderedHoverButtonStyle

---
+++ Sources/SwiftAdditions/BorderdHoverButtonStyle.swift
@@ -0,0 +1,18 @@
+import SwiftUI
+
+@available(macOS 12, iOS 16, *)
+struct BorderedHoverButtonStyle: ButtonStyle {
+	let hoverColor: Color = Color.black.opacity(0.1)
+
+	@State private var isHovered = false
+
+	public func makeBody(configuration: Self.Configuration) -> some View {
+		configuration.label
+			.buttonStyle(.borderless)
+			.padding(4)
+			.background(RoundedRectangle(cornerRadius: 4).fill(isHovered ? hoverColor : Color.clear))
+			.onHover { hover in
+				self.isHovered = hover
+			}
+	}
+}

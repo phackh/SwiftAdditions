5e54147bc273d2e20b5fc3fb76ec13250a5f2931
Patrick Hackh <mail@phackh.de>
2022-11-02 16:33:05 +0100

    Added FlexibleNavigationSplitView

---
+++ Sources/SwiftAdditions/FlexibleNavigationSplitView.swift
@@ -0,0 +1,32 @@
+//
+//  SwiftUIView.swift
+//  
+//
+//  Created by Patrick Hackh on 02.11.22.
+//
+
+import SwiftUI
+
+enum FlexibleNavigationSplitViewColumns {
+	case two
+	case three
+}
+
+@available(macOS 13, iOS 16, *)
+struct FlexibleNavigationSplitView<SidebarView: View, ContentView: View, DetailView: View>: View {
+	let columns: FlexibleNavigationSplitViewColumns
+	@Binding var visibility: NavigationSplitViewVisibility
+	
+	@ViewBuilder var sidebar: () -> SidebarView
+	@ViewBuilder var content: () -> ContentView
+	@ViewBuilder var detail: () -> DetailView
+	
+	var body: some View {
+		switch (columns) {
+		case .two:
+			NavigationSplitView(columnVisibility: $visibility, sidebar: sidebar, detail: detail)
+		case .three:
+			NavigationSplitView(columnVisibility: $visibility, sidebar: sidebar, content: content, detail: detail)
+		}
+	}
+}
